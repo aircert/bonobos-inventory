d67b545b1e1401b77e6822ec4de1ad7cdc3e2a82
Daniel Golman <aircertio@gmail.com>
2018-04-21 02:51:29 -0400

    script importing csvs and seeding db properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+@csv_text = File.read(Rails.root.join('db', 'csv', 'products.csv'))
+
+@csv = CSV.parse(@csv_text, :headers => true).map{|row| row.to_h}
+
+@rows = @csv.map{|row| row.to_h}
+
+@rows.each_with_index do |row, key|
+	Product.create!(row.to_h)
+end
+
+@csv_text = File.read(Rails.root.join('db', 'csv', 'inventory.csv'))
+
+@csv = CSV.parse(@csv_text, :headers => true).map{|row| row.to_h}
+
+@rows = @csv.map{|row| row.to_h}
+
+@rows.each_with_index do |row, key|
+	product = Product.find_by(product_key: row['product_key'])
+	product.items.create!(row.to_h)
+end
