59b502b686a4721f9fc29978c9f3efd9daf3987e
Daniel Golman <aircertio@gmail.com>
2018-04-21 00:34:02 -0400

    models generated for product.csv and inventory.csv

---
+++ db/migrate/20180421043338_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :items do |t|
+      t.integer :waist
+      t.integer :length
+      t.references :product, foreign_key: true
+      t.string :style
+      t.integer :count
+
+      t.timestamps
+    end
+  end
+end
